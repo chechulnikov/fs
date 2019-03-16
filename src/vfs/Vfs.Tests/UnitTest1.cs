5cdee0a3679a7cdff6307cfc7b7ed7a84de219b6
Maxim Chechulnikov <ilfirin.nar@gmail.com>
2019-03-16 20:20:46 +0300

    empty projects
    fix some issues

---
+++ src/vfs/Vfs.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Vfs.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ src/vfs/Vfs/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Vfs
+{
+    public class Class1
+    {
+    }
+}
